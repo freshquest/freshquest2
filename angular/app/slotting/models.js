ffa774dae824c3ca816c496472e268e7d3118dea
Paul Melnikow <email@paulmelnikow.com>
2014-04-05 20:00:56 -0400

    WIP

--- angular/app/slotting/models.js
+++ angular/app/slotting/models.js
@@ -4,4 +4,11 @@ angular.module('freshquest2')
     return $resource('/api/vendor/:id', { id: '@id' });
 })
 
+.service('MarketDay', function ($resource) {
+	var MarketDay = $resource('/api/market_day', { id: '@id' });
+	return {
+		current: MarketDay.get(),
+	};
+})
+
 ;
--- angular/app/slotting/slotting.js
+++ angular/app/slotting/slotting.js
@@ -3,12 +3,14 @@ angular.module('freshquest2')
 .config(function($stateProvider) {
     $stateProvider.state('slotting', {
         url: '/slotting',
+        controller: 'SlottingPage',
         templateUrl: '/static/app/slotting/slotting.html',
     });
 })
 
-.controller('SlottingPage', function ($scope, Vendor) {
+.controller('SlottingPage', function ($scope, Vendor, MarketDay) {
     $scope.vendors = Vendor.query();
+    $scope.market_day = MarketDay.current;
 })
 
 ;
